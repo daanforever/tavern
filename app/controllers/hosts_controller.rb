eea969cc35a2e9e3d4aeeacb61a9ac3b7249657e
dan <daan.forever@gmail.com>
2014-10-21 13:22:48 +0400

    interim

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,5 +1,5 @@
 class HostsController < ApplicationController
-  before_action :set_environment, only: [:index, :new, :create]
+  before_action :set_environment
   before_action :set_host, only: [:show, :edit, :update, :destroy]
 
   respond_to :html, :json
@@ -30,7 +30,7 @@ class HostsController < ApplicationController
     unless @host.save
       flash.now[:alert] = @host.errors
     end
-    respond_with(@host)
+    respond_with(@environment)
   end
 
   def update
@@ -41,7 +41,7 @@ class HostsController < ApplicationController
 
   def destroy
     @host.destroy
-    respond_with(@host)
+    respond_with(@environment)
   end
 
   def toggle
