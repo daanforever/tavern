d7d8f65b2b2a9e9a955384e127267b3b1d3cbc67
dan <daan.forever@gmail.com>
2014-11-25 21:31:15 +0300

    registry specs

--- spec/models/registry_spec.rb
+++ spec/models/registry_spec.rb
@@ -15,10 +15,51 @@
 require 'rails_helper'
 
 describe Registry, :type => :model do
-  let(:registry) { create(:registry) }
+  let(:registry){ create(:registry) }
+
+  let(:data) do
+    [
+      OpenStruct.new({ 
+        name: Faker::Lorem.word + '/' + Faker::Lorem.word,
+        tags: [
+          OpenStruct.new({
+            name: Faker::Lorem.word,
+            repository: OpenStruct.new({ full_name: Faker::Lorem.word })
+          })
+        ]
+      })
+    ]
+  end
+
   describe '#scan' do
     it 'not raises error' do
       expect{ registry.scan }.to_not raise_error
     end
-  end  
+  end
+
+  describe '#parse' do
+    it 'not raises error' do
+      expect{ registry.parse( data ) }.to_not raise_error
+    end
+
+    it 'returns not nil' do
+      expect( registry.parse( data ) ).to_not be_nil
+    end
+
+    it 'creates new Project' do
+      expect{ registry.parse( data ) }.to change(Project, :count).by(1)
+    end
+
+    it 'creates new Release' do
+      expect{ registry.parse( data ) }.to change(Release, :count).by(1)
+    end
+
+    it 'creates new Component' do
+      expect{ registry.parse( data ) }.to change(Component, :count).by(1)
+    end
+
+    it 'creates new Image' do
+      expect{ registry.parse( data ) }.to change(Image, :count).by(1)
+    end
+  end
 end
