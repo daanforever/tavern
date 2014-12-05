f2fa237a4039425786ad4399de8045317071eba8
dan <daan.forever@gmail.com>
2014-12-05 17:23:37 +0300

    docker-api updated to 1.15 and some specs

--- lib/docker_shell/docker_shell.rb
+++ lib/docker_shell/docker_shell.rb
@@ -7,6 +7,7 @@ class DockerShell
 
   def initialize( instance: instance )
     @instance   = instance
+    Docker.logger = Rails.logger
     @connection = Docker::Connection.new(@instance.host.url, {})
     @container  = DockerShell::Container.new( connection: @connection, instance: @instance, image: @instance.image )
     @image      = DockerShell::Image.new( connection: @connection, image: @instance.image )
--- spec/lib/docker_shell/image_spec.rb
+++ spec/lib/docker_shell/image_spec.rb
@@ -2,31 +2,46 @@
 require 'rails_helper'
 
 describe DockerShell::Image do
+  let(:instance){ create(:instance) }
+  let(:image){ DockerShell.new(instance: instance).image }
+
   describe '#exist?' do
-    let(:instance){ create(:instance) }
     context 'when image exist' do
       it 'not raises error' do
         expect(Docker::Image).to receive(:get).and_return(true)
-        image = DockerShell.new(instance: instance).image
         expect{ image.exist? }.to_not raise_error
       end
       it 'returns true' do
         expect(Docker::Image).to receive(:get).and_return(true)
-        image = DockerShell.new(instance: instance).image
         expect( image.exist? ).to eq( true )
       end
     end # when image present
     context 'when image empty' do
       it 'returns false on exception' do
         expect(Docker::Image).to receive(:exist?){ raise }
-        image = DockerShell.new(instance: instance).image
         expect( image.exist? ).to eq(false)        
       end
       it 'returns false on nil' do
         expect(Docker::Image).to receive(:exist?).and_return(false)
-        image = DockerShell.new(instance: instance).image
         expect( image.exist? ).to eq(false)        
       end
     end # when image present
   end # #exist?
+
+  describe '#pull' do
+    it 'not raises error' do
+      expect(Docker::Image).to receive(:create).and_return(true)
+      expect{ image.pull }.to_not raise_error
+    end
+
+    it 'returns true on success pull' do
+      expect(Docker::Image).to receive(:create).and_return(true)
+      expect( image.pull ).to eq(true)
+    end
+
+    it 'returns false on pull error' do
+      expect(Docker::Image).to receive(:create){ raise }
+      expect( image.pull ).to eq(false)
+    end
+  end
 end
