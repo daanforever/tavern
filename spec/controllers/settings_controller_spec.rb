7dc863b0023e3bf0e7341090a034b8efb25ca164
dan <daan.forever@gmail.com>
2014-12-12 01:32:19 +0300

    specs

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe SettingsController, :type => :controller do
   # Settings. As you add validations to Settings, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    attributes_for(:setting)
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {}
   }
 
   # This should return the minimal set of values that should be in the session
@@ -47,40 +47,26 @@ RSpec.describe SettingsController, :type => :controller do
   describe "PUT update" do
     describe "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        attributes_for(:setting)
       }
 
       it "updates the requested settings" do
         settings = Settings.create! valid_attributes
         put :update, {:id => settings.to_param, :settings => new_attributes}, valid_session
         settings.reload
-        skip("Add assertions for updated state")
+        expect(assigns(:setting).value).to eq(new_attributes[:value])
       end
 
       it "assigns the requested settings as @settings" do
         settings = Settings.create! valid_attributes
         put :update, {:id => settings.to_param, :settings => valid_attributes}, valid_session
-        expect(assigns(:settings)).to eq(settings)
+        expect(assigns(:setting)).to eq(settings)
       end
 
       it "redirects to the settings" do
         settings = Settings.create! valid_attributes
         put :update, {:id => settings.to_param, :settings => valid_attributes}, valid_session
-        expect(response).to redirect_to(settings)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the settings as @settings" do
-        settings = Settings.create! valid_attributes
-        put :update, {:id => settings.to_param, :settings => invalid_attributes}, valid_session
-        expect(assigns(:settings)).to eq(settings)
-      end
-
-      it "re-renders the 'edit' template" do
-        settings = Settings.create! valid_attributes
-        put :update, {:id => settings.to_param, :settings => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
+        expect(response).to redirect_to(settings_path)
       end
     end
   end
