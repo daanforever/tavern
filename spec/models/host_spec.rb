f448748cf967ba6bed18c25862b2eb1476834135
dan <daan.forever@gmail.com>
2014-12-12 16:01:38 +0300

    host specs

--- spec/models/host_spec.rb
+++ spec/models/host_spec.rb
@@ -15,6 +15,21 @@
 
 require 'rails_helper'
 
-RSpec.describe Host, :type => :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+describe Host, :type => :model do
+  let(:host){ create(:host) }
+  describe '#refresh' do
+    it 'not raises error' do
+      expect( Docker::Container ).to receive( :all ).and_return({})
+      expect{ host.refresh }.to_not raise_error
+    end
+    it 'returns array' do
+      containers = [ OpenStruct.new({id: rand(9999)}) ]
+      container = double( Docker::Container )
+      info = { 'Config' => { 'Image' => Faker::Lorem.word }, 'State' => { 'Running' => true } }
+      expect( Docker::Container ).to receive( :all ).and_return(containers)
+      expect( Docker::Container ).to receive( :get ).and_return(container)
+      expect( container ).to receive( :info ).twice.and_return(info)
+      expect( host.refresh ).to be_a(Array)
+    end
+  end  
 end
