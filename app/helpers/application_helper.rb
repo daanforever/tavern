62e6b5fad03565f6ead378e2409932d106bb5742
dan <daan.forever@gmail.com>
2015-01-16 18:02:25 +0300

    new section Help

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def active_if(controller)
-    { class: controller_name.to_s == controller ? 'active' : 'inactive' }
+  def active_if(controller, action = nil)
+    if (controller == controller_name) and (action.nil? or action == action_name)
+      { class: 'active' }
+    else
+      { class: 'inactive' }
+    end
   end
 end
