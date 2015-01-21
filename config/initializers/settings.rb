abee10a15d55c6418ad898a96b78fc0cb5ee197c
dan <daan.forever@gmail.com>
2015-01-21 13:09:10 +0300

    many changes

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,6 +1,15 @@
 Rails.application.config.after_initialize do
   begin
     Settings.docker.timeout? || Settings.docker.timeout = 10
+    Settings.form.horizontal = {
+      html: {class: 'form-horizontal'}, 
+      wrapper: :horizontal_form, 
+      wrapper_mappings: {
+          check_boxes: :horizontal_radio_and_checkboxes, 
+          radio_buttons: :horizontal_radio_and_checkboxes, 
+          file: :horizontal_file_input, boolean: :horizontal_boolean 
+      }
+    }
   rescue
     Rails.logger.warn('Settings not defined')
   end
